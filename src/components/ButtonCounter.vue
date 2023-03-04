c9538df42249e4fdb06a2c052a89461f114d2cdb
Teja Thota <74037779+Teja110902@users.noreply.github.com>
2023-03-04 11:52:27 +0530

    Add Components

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="home">
+        <!-- <h1 class="myClass">Hello, world!</h1> -->
+        <!-- Two way binding -->
+        <!-- <input type="text" v-model="user" placeholder="Enter your name..." /> -->
+        <button @click="sayHello()">Click me!</button>
+        <h1>You pressed the button {{ counter }} times!</h1>
+        <!-- One way binding -->
+        <!-- <h1 class="myClass">Hello, {{ user }}!</h1> -->
+
+        <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    data() {
+        return {
+            // user: "Teja"
+            counter: 0
+        }
+    },
+    methods: {
+        sayHello() {
+            console.log("Hello from method!");
+            this.counter++;
+        }
+    }
+    // name: 'HomeView',
+    // components: {
+    //   HelloWorld
+    // }
+}
+</script>
+
+<style>
+.myClass {
+    color: red;
+}
+</style>
